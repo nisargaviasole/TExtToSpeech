54b971b52f0fec201d8684463ec0fa15d868452c
nisargaviasole <nisarg.jadhav@aviasole.com>
2025-02-19 13:04:33 +0530

    changes

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def transcribe():
     return jsonify({"transcription": transcription}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
